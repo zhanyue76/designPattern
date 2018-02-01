7bb95d2e395c25488b21f97d0a26f9eb10513ba9
songjy@hjsoft.com.cn <songjy@hjsoft.com.cn>
2018-02-01 10:00:18 +0800

    快速排序：左右都变化

---
+++ src/main/java/com/songjy/written/sort/MyQuickSort.java
@@ -0,0 +1,44 @@
+package com.songjy.written.sort;
+
+public class MyQuickSort {
+
+	public static void sort(int[] arrs,int begin,int end){
+		if(begin>=end) return;
+		int left = begin;
+		int right = end;
+		int index = arrs[left];
+		while(left<right){
+			while(left<right && arrs[right]>index){
+				right--;
+			}
+			if(left<right){
+				arrs[left]=arrs[right];
+				left++;
+			}
+			while(left<right&&arrs[left]<index){
+				left++;
+			}
+			if(left<right){
+				arrs[right]=arrs[left];
+				right--;
+			}
+		}
+		arrs[left]=index;
+		sort(arrs,begin,left-1);
+		sort(arrs,left+1,end);
+		
+	}
+	
+	public static void print(int[] ar){
+		for(int i=0;i<ar.length;i++){
+			System.out.print(ar[i]+",");
+		}
+	}
+	
+	public static void main(String[] args) {
+		int[] arrs = {15,22,13,9,16,33,15,23,18,4,33,25,14};
+		sort(arrs,0,arrs.length-1);
+		print(arrs);
+	}
+
+}
